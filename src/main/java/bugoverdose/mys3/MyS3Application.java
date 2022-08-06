1f75e1a2bf39882eccfe4b3d4dd7abff14c10d95
Jeong Jinwoo <jwjeong96@gmail.com>
2022-08-06 20:04:59 +0900

    feat: setup spring boot project

---
+++ src/main/java/bugoverdose/mys3/MyS3Application.java
@@ -0,0 +1,12 @@
+package bugoverdose.mys3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyS3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyS3Application.class, args);
+    }
+}
---
+++ src/test/java/bugoverdose/mys3/MyS3ApplicationTests.java
@@ -0,0 +1,12 @@
+package bugoverdose.mys3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyS3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
