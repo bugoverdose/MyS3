6935f1efb1e38ef5639462c8be9d0a4a183780ce
Jeong Jinwoo <jwjeong96@gmail.com>
2022-08-07 00:34:22 +0900

    feat: response with JSON on exception handling

--- src/main/java/bugoverdose/mys3/exception/ExceptionListener.java
+++ src/main/java/bugoverdose/mys3/exception/ExceptionListener.java
@@ -1,5 +1,6 @@
 package bugoverdose.mys3.exception;
 
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,23 +12,38 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class ExceptionListener {
 
     @ExceptionHandler
-    public ResponseEntity<String> handle(Exception e) {
+    public ResponseEntity<ErrorResponse> handle(Exception e) {
         log.error("Internal Server Error: ", e);
-        return new ResponseEntity<>("예상치 못한 에러가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(new ErrorResponse("예상치 못한 에러가 발생했습니다."), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler
-    public ResponseEntity<String> handle(InvalidRequestException e) {
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<ErrorResponse> handle(InvalidRequestException e) {
+        return new ResponseEntity<>(new ErrorResponse(e), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler
-    public ResponseEntity<String> handle(UnauthenticatedException e) {
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
+    public ResponseEntity<ErrorResponse> handle(UnauthenticatedException e) {
+        return new ResponseEntity<>(new ErrorResponse(e), HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler
-    public ResponseEntity<String> handle(NotFoundException e) {
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    public ResponseEntity<ErrorResponse> handle(NotFoundException e) {
+        return new ResponseEntity<>(new ErrorResponse(e), HttpStatus.NOT_FOUND);
+    }
+
+    @Getter
+    private static class ErrorResponse {
+
+        Boolean ok = false;
+        String error;
+
+        ErrorResponse(String message) {
+            this.error = message;
+        }
+
+        ErrorResponse(RuntimeException exception) {
+            this(exception.getMessage());
+        }
     }
 }
