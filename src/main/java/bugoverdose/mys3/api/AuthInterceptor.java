44f04e8a5f11e7533fa6b5c775532e9fc2dbfbb6
Jeong Jinwoo <jwjeong96@gmail.com>
2022-08-07 00:04:10 +0900

    feat: implement minimum authentication for uploading images
    - specific authorization code needs to be included at Authorization header

---
+++ src/main/java/bugoverdose/mys3/api/AuthInterceptor.java
@@ -0,0 +1,36 @@
+package bugoverdose.mys3.api;
+
+import bugoverdose.mys3.exception.UnauthenticatedException;
+import bugoverdose.mys3.service.AuthService;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class AuthInterceptor implements HandlerInterceptor {
+
+    private static final String AUTHORIZATION = "Authorization";
+
+    private final AuthService authService;
+
+    public AuthInterceptor(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
+                             Object handler) {
+        String method = request.getMethod();
+        if (method.equals(HttpMethod.GET.name()) || method.equals(HttpMethod.OPTIONS.name())) {
+            return true;
+        }
+        try {
+            authService.validate(request.getHeader(AUTHORIZATION));
+            return true;
+        } catch (UnauthenticatedException e) {
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            return false;
+        }
+    }
+}
