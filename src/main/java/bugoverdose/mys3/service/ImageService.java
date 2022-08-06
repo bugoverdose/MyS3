02c76e53472a62c78ed88955ddeef89d11f7a6f0
Jeong Jinwoo <jwjeong96@gmail.com>
2022-08-06 21:36:22 +0900

    feat: configure static directory based on root directory
    - always save and manage uploaded image files at the same directory no matter where you run the .jar file

--- src/main/java/bugoverdose/mys3/service/ImageService.java
+++ src/main/java/bugoverdose/mys3/service/ImageService.java
@@ -5,6 +5,7 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
+import javax.swing.filechooser.FileSystemView;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -14,8 +15,9 @@ public class ImageService {
 
     private final String filePathFormat;
 
-    public ImageService(@Value("${image.storage.root-directory}") String rootDirectory) {
-        this.filePathFormat = rootDirectory + "/%s/%s.png";
+    public ImageService(@Value("${image.storage.root-directory}") String storageDirectory) {
+        String rootDirectory = FileSystemView.getFileSystemView().getHomeDirectory().getAbsolutePath();
+        this.filePathFormat = rootDirectory + "/" + storageDirectory + "/%s/%s.png";
     }
 
     public void upload(UploadImageCommand command) {
