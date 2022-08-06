467705420800106e6d42159dd1c93f12ce259ac4
Jeong Jinwoo <jwjeong96@gmail.com>
2022-08-07 01:12:38 +0900

    feat: throw exception on uploading non-image or gif files

--- src/main/java/bugoverdose/mys3/image/dto/UploadImageCommand.java
+++ src/main/java/bugoverdose/mys3/image/dto/UploadImageCommand.java
@@ -1,7 +1,9 @@
 package bugoverdose.mys3.image.dto;
 
+import bugoverdose.mys3.exception.InvalidRequestException;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.multipart.MultipartFile;
 
 @NoArgsConstructor
@@ -13,11 +15,25 @@ public class UploadImageCommand {
     private MultipartFile uploadedImageFile;
 
     public UploadImageCommand(String uploadPath, String fileName, MultipartFile uploadedImageFile) {
+        validateImageFile(uploadedImageFile);
         if (fileName.isBlank()) {
             fileName = uploadedImageFile.getOriginalFilename();
         }
         this.uploadPath = uploadPath.strip().toLowerCase();
         this.fileName = fileName.strip().toLowerCase();
         this.uploadedImageFile = uploadedImageFile;
+    }
+
+    private void validateImageFile(MultipartFile uploadedImageFile) {
+        if (uploadedImageFile == null || uploadedImageFile.isEmpty()) {
+            throw new InvalidRequestException("업로드할 파일이 존재하지 않습니다.");
+        }
+        String contentType = uploadedImageFile.getContentType();
+        if (contentType == null || !contentType.startsWith("image")) {
+            throw new InvalidRequestException("이미지 파일만 업로드 가능합니다.");
+        }
+        if (contentType.equals(MediaType.IMAGE_GIF_VALUE)) {
+            throw new InvalidRequestException("GIF 파일은 업로드할 수 없습니다.");
+        }
     }
 }
