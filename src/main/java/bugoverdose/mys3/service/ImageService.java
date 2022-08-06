13c06e9accb7e8bcec6bd792b5ffcda873c1c915
Jeong Jinwoo <jwjeong96@gmail.com>
2022-08-06 21:24:15 +0900

    refactor: configure and inject root directory value via application.yml
    - for easier and more readable configuration

--- src/main/java/bugoverdose/mys3/service/ImageService.java
+++ src/main/java/bugoverdose/mys3/service/ImageService.java
@@ -5,21 +5,26 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class ImageService {
 
-    private static final String FILENAME_FORMAT = "static/images/%s/%s";
+    private final String filePathFormat;
+
+    public ImageService(@Value("${image.storage.root-directory}") String rootDirectory) {
+        this.filePathFormat = rootDirectory + "/%s/%s.png";
+    }
 
     public void upload(UploadImageCommand command) {
+        MultipartFile uploadedImageFile = command.getUploadedImageFile();
         String uploadPath = command.getUploadPath();
         String fileName = command.getFileName();
-        MultipartFile uploadedImageFile = command.getUploadedImageFile();
         try {
             BufferedImage uploadedImage = ImageIO.read(uploadedImageFile.getInputStream());
-            File outputFile = new File(String.format(FILENAME_FORMAT, uploadPath, fileName));
+            File outputFile = new File(String.format(filePathFormat, uploadPath, fileName));
             createParentDirectoryIfNew(outputFile);
             ImageIO.write(uploadedImage, "png", outputFile);
         } catch (IOException e) {
