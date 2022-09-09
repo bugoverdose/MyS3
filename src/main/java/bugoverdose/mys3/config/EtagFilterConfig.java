c0e2c3ee7102d85987eaa152f8593de17e6f3190
Jeong Jinwoo <jwjeong96@gmail.com>
2022-09-10 01:50:21 +0900

    feat(EtagFilterConfig): generate and compare ETag header based on file content

---
+++ src/main/java/bugoverdose/mys3/config/EtagFilterConfig.java
@@ -0,0 +1,18 @@
+package bugoverdose.mys3.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
+
+@Configuration
+public class EtagFilterConfig {
+
+    @Bean
+    public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
+        FilterRegistrationBean<ShallowEtagHeaderFilter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(new ShallowEtagHeaderFilter());
+        registration.addUrlPatterns("/images/*");
+        return registration;
+    }
+}
